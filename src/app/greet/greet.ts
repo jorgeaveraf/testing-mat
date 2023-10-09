dc3d34a9f15a55a4cd5c237b6de2facd0316396c
Jorge Vera <112434926+jorgeaveraf@users.noreply.github.com>
2023-10-09 09:56:09 -0600

    Delete src/app/greet directory

--- src/app/greet/greet.ts
+++
@@ -1,9 +0,0 @@
-function greet(input: string): string {
-    if (input != "") {
-        return "Hello, " + input + "!";
-    } else {
-        return "Hello, World!";
-    }
-}
-
-export { greet };
