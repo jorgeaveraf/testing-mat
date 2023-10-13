20f45bb38c23543433757f9024b9fb33d3e05e2e
JorgeVera <jorgeaveraf@gmail.com>
2023-10-13 11:21:39 -0600

    add stddev #7

---
+++ src/app/services/media.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MediasService {
+  private apiURLProxySize = 'https://api-node-service-danielaaer.cloud.okteto.net/proxy';
+  private apiUrlDevHours = 'https://api-node-service-danielaaer.cloud.okteto.net/dev';
+
+  constructor(private http: HttpClient) { }
+
+  getProxySize(): Observable<number[]> {
+    return this.http.get<{data: number[]}>(this.apiURLProxySize).pipe(
+      map(response => response.data)
+    );
+  }
+
+  getDevHours(): Observable<number[]> {
+    return this.http.get<{data: number[]}>(this.apiUrlDevHours).pipe(
+      map(response => response.data)
+    );
+  }
+  
+}
---
+++ src/app/stddev/stddev.component.spec.ts
@@ -0,0 +1,39 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { StddevComponent } from './stddev.component';
+import { MediasService } from '../services/media.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MediaComponent } from '../media/media.component';
+
+describe('StddevComponent', () => {
+  let component: StddevComponent;
+  let fixture: ComponentFixture<StddevComponent>;
+  let service: MediasService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MediasService, MediaComponent],
+      declarations: [StddevComponent],
+      imports: [HttpClientModule]
+    });
+    service = TestBed.inject(MediasService);
+    fixture = TestBed.createComponent(StddevComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should calculate the standard deviation correctly for ProxySize', () => {
+    const expectedStdDev = 542.6723136479325;
+
+    component.arr = expectedStdDev;
+
+    expect(component.arr).toEqual(expectedStdDev);
+  });
+
+  it('should calculate the standard deviation correctly for DevHours', () => {
+    const expectedStdDev = 59.06106670218546;
+
+    component.arr2 = expectedStdDev;
+
+    expect(component.arr2).toEqual(expectedStdDev);
+  });
+});
